c0370536a2433c8929943c8aaa40a59f77781364
Patrick Kosterman <patrick@passkit.com>
2017-08-21 19:37:50 +0700

    Updated sample code to reflect latest API changes.

---
+++ v2PassKitSDKExample/v2PassKitSDKExample/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * PassKit C# Wrapper for PassKit API v2:
+ * https://dev.passkit.net/v2
+ */
+using System;
+using v2PassKitSDK;
+
+namespace v2PassKitSDKExample
+{
+	class Example
+	{
+		static void Main(string[] args)
+		{
+			PassKit pk = new PassKit("<< your-api-key >>", "<< your-api-secret >>");
+
+			try
+			{
+				/* Create a Pass */
+				Pass p1 = new Pass();
+				p1.templateName = "<< your-template-name >>";
+
+				string pid = pk.createPass(p1);
+				Console.WriteLine("https://q.passkit.net/p-" + pid);
+
+				/* Retrieve a Pass */
+                Pass p2 = pk.retrievePassById(pid);
+                Console.WriteLine(p2.campaignName);
+
+                /* Search for a Pass */
+                Pass p3 = new Pass();
+                p3.passbook = new PassPassbook();
+                p3.passbook.fgColor = "#ffffff"; // set text colors to white
+                p3.passbook.labelColor = "#ffffff"; // set text colors to white
+
+                string pidUpdate = pk.updatePass(pid, p3);
+                Console.WriteLine("https://q.passkit.net/p-" + pidUpdate);
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.Message);
+			}
+		}
+	}
+}
